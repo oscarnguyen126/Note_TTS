e2abcba4e4313278392b6b932aa42314756ae921
Duong <duong6313@gmail.com>
2024-05-28 09:05:07 +0700

    Modify model

--- noteapi/NA/models.py
+++ noteapi/NA/models.py
@@ -1,34 +1,37 @@
 from django.db import models
+from uuid import uuid4 as UUID4
 from django.utils.timezone import now as djnow
-
-class User(models.Model):
-    uuid = models.AutoField(primary_key=True)
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
+from django.contrib.auth.hashers import make_password
+class User(AbstractUser):
+    uuid = models.UUIDField(default=UUID4,max_length=64,editable=False,unique=True, primary_key=True,)
     user_name = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
-    created_at = models.DateTimeField(default=djnow, editable=False)
-    updated_at = models.DateTimeField(default=djnow, editable=False)
+    created_at = models.DateTimeField( editable=True,blank=False, null=False,default=djnow,help_text=_('Thời điểm tạo'),)
+    updated_at = models.DateTimeField(editable=True,blank=False,null=True,default=djnow,help_text=_('Thời điểm cập nhật'),)
 
     def __str__(self):
         return self.user_name
 
 class Note(models.Model):
-    note_id = models.AutoField(primary_key=True)
+    uuid = models.UUIDField(default=UUID4,max_length=64,editable=False,unique=True, primary_key=True,)
     folder_id = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.CharField(max_length=1000)
-    updated_at = models.DateTimeField(default=djnow(),editable=False)
-    created_at = models.DateTimeField(default=djnow(),editable=False)
+    created_at = models.DateTimeField(editable=True, blank=False, null=False, default=djnow,help_text=_('Thời điểm tạo'), )
+    updated_at = models.DateTimeField(editable=True, blank=False, null=True, default=djnow,help_text=_('Thời điểm cập nhật'), )
 
     def __str__(self):
         return self.content
 
 class Folder(models.Model):
-    folder_id = models.AutoField(primary_key = True)
+    uuid = models.UUIDField(default=UUID4,max_length=64,editable=False,unique=True, primary_key=True,)
     folder_name = models.CharField(max_length=200)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    updated_at = models.DateTimeField(default=djnow(),editable=False)
-    created_at = models.DateTimeField(default=djnow(),editable=False)
+    created_at = models.DateTimeField(editable=True, blank=False, null=False, default=djnow,help_text=_('Thời điểm tạo'), )
+    updated_at = models.DateTimeField(editable=True, blank=False, null=True, default=djnow,help_text=_('Thời điểm cập nhật'), )
 
     def __str__(self):
         return self.folder_name
